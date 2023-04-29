4b378b84c1f0c12f7b2ba2258214f629be0c637e
zhyrgalbek <zhyrgalbek.kamalov@gmail.com>
2023-04-29 13:54:58 +0600

    add swiper

--- script.js
+++ script.js
@@ -46,6 +46,70 @@ $(function () {
         ]
     })
 
+    const swiper = new Swiper(".mySwiper", {
+        effect: "coverflow",
+        grabCursor: true,
+        centeredSlides: true,
+        slidesPerView: 'auto',
+        loop: true,
+        coverflowEffect: {
+            rotate: 0,
+            stretch: 0,
+            depth: 350,
+            modifier: 3,
+            slideShadows: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        // breakpoints: {
+        //     // when window width is >= 320px
+        //     320: {
+        //         slidesPerView: 'auto',
+        //         spaceBetween: 20,
+        //         modifier: 1,
+        //     },
+        //     768: {
+
+
+        //     }
+        // }
+    });
+
+    const swiper2 = new Swiper(".trust-swiper", {
+        slidesPerView: 3,
+        spaceBetween: 30,
+        // loop: true,
+        pagination: {
+            el: ".swiper-pagination",
+            clickable: true,
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 20
+            },
+            1200: {
+                slidesPerView: 3,
+                spaceBetween: 20
+            },
+            768: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+        }
+    });
+
     // let number = document.querySelector('.count');
     // let numberTop = number.getBoundingClientRect().top;
     // let start = +number.innerHTML;
@@ -75,10 +139,6 @@ $(function () {
     //     })
     // })
 })
-
-
-
-
 
 
 let hamburger = document.querySelector('.hamburger');
