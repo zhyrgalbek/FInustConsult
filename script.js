eaf70013cea08d5e1e2429a8837f6e0467c51c21
pib <zhyrgalbek.kamalov@gmail.com>
2023-05-12 18:23:44 +0600

    add new bloks

--- script.js
+++ script.js
@@ -108,17 +108,39 @@ $(function () {
     //     }
     // })
 
-    // $('.count').each(function () {
-    //     $(this).prop('Counter', 0).animate({
-    //         Counter: $(this).text()
-    //     }, {
-    //         duration: 4000,
-    //         easing: 'swing',
-    //         step: function (now) {
-    //             $(this).text(Math.ceil(now));
-    //         }
-    //     })
-    // })
+    $('.count1').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 2000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        })
+    })
+    $('.count2').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 1500,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        })
+    })
+    $('.count3').each(function () {
+        $(this).prop('Counter', 0).animate({
+            Counter: $(this).text()
+        }, {
+            duration: 4000,
+            easing: 'swing',
+            step: function (now) {
+                $(this).text(Math.ceil(now));
+            }
+        })
+    })
 })
 
 
